d119d9cdce31f48d00b0da3b58a6fcdd991b81b1
junghong <junghong@ninewatt.com>
2021-07-20 11:34:25 +0900

    공유

--- examples/website/geojson/app.js
+++ examples/website/geojson/app.js
@@ -2,35 +2,15 @@ import React, { useState, useRef, useCallback} from 'react';
 import {render} from 'react-dom';
 import { StaticMap, Popup } from 'react-map-gl';
 import DeckGL from '@deck.gl/react';
-import {GeoJsonLayer, PolygonLayer} from '@deck.gl/layers';
+import axios from 'axios';
+import { GeoJsonLayer, TextLayer } from '@deck.gl/layers';
 import { MapboxLayer } from '@deck.gl/mapbox';
-import {LightingEffect, AmbientLight, _SunLight as SunLight} from '@deck.gl/core';
 import {scaleThreshold} from 'd3-scale';
 
 import { DATA_URL } from './data';
-
-// Source data GeoJSON
-// const DATA_URL =
-//   'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/geojson/vancouver-blocks.json'; // eslint-disable-line
-
-export const COLOR_SCALE = scaleThreshold()
-  .domain([-0.6, -0.45, -0.3, -0.15, 0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05, 1.2])
-  .range([
-    [65, 182, 196],
-    [127, 205, 187],
-    [199, 233, 180],
-    [237, 248, 177],
-    // zero
-    [255, 255, 204],
-    [255, 237, 160],
-    [254, 217, 118],
-    [254, 178, 76],
-    [253, 141, 60],
-    [252, 78, 42],
-    [227, 26, 28],
-    [189, 0, 38],
-    [128, 0, 38]
-  ]);
+import { SEOUL_GU_LATLNG } from './seoulGu';
+import { SEOUL } from "./seoul";
+import { SELECTED_BUILDINGS } from "./seoul_buildings";
 
 const INITIAL_VIEW_STATE = {
   latitude: 37.498042,
@@ -41,27 +21,15 @@ const INITIAL_VIEW_STATE = {
   bearing: 0
 };
 
-// const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
-// const MAP_STYLE = 'https://api.mapbox.com/styles/v1/junghong/ckquispiyd7jf17nxwqux3bkz.html?fresh=true&title=copy&access_token=pk.eyJ1IjoianVuZ2hvbmciLCJhIjoiY2twbnBiZTA3MXQ3NjJ2bHJodHJmN2oyYSJ9.QW798Vk3hHy6I158OoHDzg';
+const OPENWEATHERMAP_API_KEY = "ab8dee082c400b1dbffa53fe7014099b";
+const AIRVISUAL_API_KEY = "d9767932-4315-43c6-88d0-1ee90aa3ae30";
 
-const ambientLight = new AmbientLight({
-  color: [255, 255, 255],
-  intensity: 1.0
-});
+const getTooltip = ({object}) => {
+  
+  console.log(object)
 
-const dirLight = new SunLight({
-  timestamp: Date.UTC(2019, 7, 1, 22),
-  color: [255, 255, 255],
-  intensity: 1.0,
-  _shadow: true
-});
-
-const landCover = [[[127.11, 37.470], [127.11, 37.535], [126.99, 37.535], [126.99, 37.470]]];
-
-
-function getTooltip({object}) {
   return (
-    object && {
+    object && object.type && {
       html: `\
         <div><b>건물정보</b></div>
         <div>UFID: ${object.properties.UFID}</div>
@@ -75,13 +43,8 @@ function getTooltip({object}) {
         <div>RDNM(도로명): ${object.properties.RDNM}</div>
         <div>BONU(건물번호본번): ${object.properties.BONU}</div>
         <div>BUNU(건물번호부번): ${object.properties.BUNU}</div>
-        <div>POST(우편번호): ${object.properties.POST}</div>
-        <div>SCLS(통합코드): ${object.properties.SCLS}</div>
-        <div>FMTA(제작정보): ${object.properties.FMTA}</div>
         <div>SHAPE_Area: ${object.properties.SHAPE_Area}</div>
         <div>SHAPE_Leng: ${object.properties.SHAPE_Leng}</div>
-        <div>geom_Area: ${object.properties.geom_Area}</div>
-        <div>geom_Lengt: ${object.properties.geom_Lengt}</div>
       `
     }
   );
@@ -90,41 +53,66 @@ function getTooltip({object}) {
 export default function App({data = DATA_URL}) {
 
   const [glContext, setGLContext] = useState();
+  
   const deckRef = useRef(null);
   const mapRef = useRef(null);
-
-  const [effects] = useState(() => {
-    const lightingEffect = new LightingEffect({ambientLight, dirLight});
-    lightingEffect.shadowColor = [0, 0, 0, 0];
-    return [lightingEffect];
-  });
-
-  const [hoverInfo, setHoverInfo] = useState();
 
   const onMapLoad = useCallback(() => {
     const map = mapRef.current.getMap();
     const deck = deckRef.current.deck;
 
-    console.log(deck)
-
-    // You must initialize an empty deck.gl layer to prevent flashing
     map.addLayer(
-      // This id has to match the id of the deck.gl layer
       new MapboxLayer({ id: "my-scatterplot", deck }),
-      // Optionally define id from Mapbox layer stack under which to add deck layer
-      // 'beforeId'
     );
   }, []);
 
+
+  /* Adding weather information into the GU element */
+  let seoulGu = SEOUL_GU_LATLNG.DATA;
+  
+  const weatherAdded = async (seoulGu, layers) => {
+    seoulGu.map(gu => {
+      let { lat, lng } = gu;
+      let config = {
+        method: 'GET',
+        // url: `http://api.airvisual.com/v2/nearest_city?lat=${lat}&lon=${lng}&key=${AIRVISUAL_API_KEY}`,
+        url: `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${OPENWEATHERMAP_API_KEY}&units=metric`,
+        header: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Max-Age": 3600,
+          "Access-Control-Allow-Headers": "Origin,Accept, X-Requested-With,Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+        },
+      }
+
+      let coordArr = [parseFloat(gu.lng), parseFloat(gu.lat)];
+      gu.coordinates = coordArr;
+
+      axios(config)
+      .then(async function(response) {
+        gu.name = await (response.data.main.temp).toString();
+        gu.weather = response.data;
+      })
+      .catch(function(error) {
+        gu.weather = "Error Occured!" + error;
+      })
+    })
+
+    return layers.push(
+      new TextLayer({
+        id: 'text-layer',
+        data: seoulGu,
+        pickable: true,
+        getPosition: d => d.coordinates,
+        getText: d => d.name,
+        getSize: 80,
+        getAngle: 0,
+        getTextAnchor: 'middle',
+        getAlignmentBaseline: 'center'
+      })
+    );
+  }
+
   const layers = [
-    // only needed when using shadows - a plane for shadows to drop on
-    // new PolygonLayer({
-    //   id: 'ground',
-    //   data: landCover,
-    //   stroked: false,
-    //   getPolygon: f => f,
-    //   getFillColor: [0, 0, 0, 0]
-    // }),
     new GeoJsonLayer({
       id: 'geojson',
       data,
@@ -134,12 +122,49 @@ export default function App({data = DATA_URL}) {
       filled: true,
       extruded: true,
       wireframe: true,
-      getElevation: f => Math.sqrt(f.properties.NMLY) * 5,
-      getFillColor: [200, 200, 200],
+      getElevation: f => f.properties.NMLY * 5,
+      getFillColor: d => d.properties.selected ? [100, 105, 155] : [55, 205, 155],
+      // getFillColor: [200, 200, 200],
       getLineColor: [10, 10, 10],
+      pickable: true,
+    }),
+
+    new GeoJsonLayer({
+      id: 'geojson',
+      data: SELECTED_BUILDINGS,
+      opacity: 0.5,
+      autoHighlight: true,
+      stroked: false,
+      filled: true,
+      extruded: true,
+      wireframe: true,
+      getElevation: f => f.properties.A32 * 5,
+      getFillColor: d => d.properties.selected ? [200, 200, 200] : [200, 200, 200],
+      // getFillColor: [200, 200, 200],
+      getLineColor: [10, 10, 10],
+      pickable: true,
+    }),
+
+    new GeoJsonLayer({
+      id: 'geojson2',
+      data: SEOUL,
+      opacity: 0.4,
+      autoHighlight: true,
+      stroked: false,
+      filled: false,
+      extruded: true,
+      wireframe: true,
+      getElevation: 0,
+      lineWidthScale: 40,
+      lineWidthMinPixels: 5,
+      getFillColor: [160, 160, 180, 190],
+      getLineColor: [50, 10, 10],
       pickable: true,
     })
   ];
+
+
+  weatherAdded(seoulGu, layers);
 
   return (
     <DeckGL
@@ -155,11 +180,6 @@ export default function App({data = DATA_URL}) {
         stencil: true
       }}
     >
-      {hoverInfo && (
-        <div style={{position: 'absolute', zIndex: 1, pointerEvents: 'none', left: hoverInfo.x, top: hoverInfo.y}}>
-          {hoverInfo? console.log(hoverInfo) :"null"}
-        </div>
-      )}
       {glContext && (
         /* This is important: Mapbox must be instantiated after the WebGLContext is available */
         <StaticMap
